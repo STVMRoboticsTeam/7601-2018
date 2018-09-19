6c3182900621b7bdcedce89121ebf95be9b49dea
bdonnz <bdonnelly@students.stvm.com>
2018-09-19 15:23:11 -0400

    Added code to test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,8 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by CST on 9/5/2018.
  */
+@TeleOp(name = "Test")
+public class Test extends LinearOpMode {
 
-public class Test {
+    @Override
+    public void runOpMode() {
+        
+    }
 }
