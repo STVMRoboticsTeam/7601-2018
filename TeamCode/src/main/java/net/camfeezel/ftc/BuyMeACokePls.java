568d77c066480d769e6800496fccfff57ceef6de
Cameron Feezel <camfeezel@gmail.com>
2018-12-01 10:13:39 -0500

    Added lift motor

--- TeamCode/src/main/java/net/camfeezel/ftc/BuyMeACokePls.java
+++ TeamCode/src/main/java/net/camfeezel/ftc/BuyMeACokePls.java
@@ -7,12 +7,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "BuyMeACokePls")
 public class BuyMeACokePls extends LinearOpMode {
 
-    private DcMotor left,right;
+    private DcMotor left,right,lift;
 
     @Override
     public void runOpMode() {
         left = hardwareMap.dcMotor.get("left");
         right = hardwareMap.dcMotor.get("right");
+        try {
+            lift = hardwareMap.dcMotor.get("lift");
+        } catch (Exception exc) {
+            telemetry.addLine("-- No Lift Motor --");
+        }
         telemetry.addLine("Made by 8497 because 7601 can't make their own code.");
         telemetry.update();
         waitForStart();
@@ -21,12 +26,18 @@ public class BuyMeACokePls extends LinearOpMode {
             float lx = gamepad1.left_stick_x;
             if(Math.abs(ly) > 0.1f) {
                 left.setPower(ly*ly*ly);
-                right.setPower(-(ly*ly*ly));
+                right.setPower(ly*ly*ly);
             } else if(Math.abs(lx) > 0.1f) {
-                left.setPower(lx);
+                left.setPower(-lx);
                 right.setPower(lx);
             }
-
+            if(lift != null) {
+                if(gamepad1.y) {
+                    lift.setPower(0.2f);
+                } else if(gamepad1.a) {
+                    lift.setPower(-0.2f);
+                }
+            }
         }
     }
 }
